d6f858391e87f853cfe0a0bee1995e7379cfa4cb
fjaquet <fjaquet@outlook.fr>
2026-03-14 20:02:42 +0100

    test some checks

--- src/components/Offer.jsx
+++ src/components/Offer.jsx
@@ -1,4 +1,5 @@
 import { Link } from "react-router-dom";
+import defaultAvatar from "../assets/img/default-avatar.png";
 
 const Offer = ({ offer }) => {
   return (
@@ -6,7 +7,7 @@ const Offer = ({ offer }) => {
       <div className="offer-card__owner">
         <img
           className="offer-card__avatar"
-          src={offer.owner.account.avatar.url}
+          src={offer.owner.account.avatar.url2 || defaultAvatar}
           alt=""
         />
         <p className="offer-card__username">{offer.owner.account.username}</p>
@@ -14,8 +15,12 @@ const Offer = ({ offer }) => {
       <img className="offer-card__image" src={offer.product_image.url} alt="" />
       <div className="offer-card__details">
         <p className="offer-card__price">{offer.product_price} €</p>
-        <p className="offer-card__size">{offer.product_details[1].TAILLE}</p>
-        <p className="offer-card__brand">{offer.product_details[0].MARQUE}</p>
+        <p className="offer-card__size">
+          {offer.product_details.find((elt) => elt.TAILLE)?.TAILLE ?? ""}
+        </p>
+        <p className="offer-card__brand">
+          {offer.product_details.find((elt) => elt.MARQUE)?.MARQUE ?? ""}
+        </p>
       </div>
     </Link>
   );
